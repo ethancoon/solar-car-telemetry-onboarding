f0af900d78efce92cd7af3971130b44ba2e63f46
Ethan <eethanccoon@gmail.com>
2023-09-21 13:09:05 -0400

    Attempted socket.IO connection

--- client/src/Display.tsx
+++ client/src/Display.tsx
@@ -1,8 +1,31 @@
+import { useEffect } from 'react';
 import './Display.css'
+import { io } from "socket.io-client";
+
 
 // To-do: take in data from the server and display it through this component
 
+const socket = io("http://localhost:5000")
+
+interface Update {
+    name: string;
+    number: number;
+    timestamp: string;
+  }
+
 export default function Display() {
+    useEffect(() => {
+        socket.on("connect", () => {
+            console.log("Connected to server")
+        })
+        socket.on("update", (update: Update) => {
+            console.log(update)
+        })
+        return () => {
+            socket.disconnect()
+        }
+    })
+
     return (
         <div className="display-wrapper">
             <h1>Display</h1>
