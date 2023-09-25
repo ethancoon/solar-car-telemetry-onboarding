c6ddb95a7319ad05a7928de66dcf8a0fe40d4e5f
Ethan <eethanccoon@gmail.com>
2023-09-25 18:02:13 -0400

    Data is now displayed correctly in reverse-chronological order, tested using mockData

--- client/src/Display.tsx
+++ client/src/Display.tsx
@@ -1,37 +1,68 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Display.css'
 import { io } from "socket.io-client";
 
 
 // To-do: take in data from the server and display it through this component
 
-const socket = io("http://localhost:5000")
 
-interface Update {
+
+interface Message {
     name: string;
-    number: number;
-    timestamp: string;
-  }
+    value: number;
+    timestamp: number;
+  };
 
-export default function Display() {
+// const mockData: Message[] = [{
+//     name: "Temperature",
+//     value: 23,
+//     timestamp: 1628779091
+// }, {
+//     name: "Humidity",
+//     value: 50,
+//     timestamp: 1628779092
+// }, {
+//     name: "Humidity",
+//     value: 50,
+//     timestamp: 1628779094
+// }, {
+//     name: "Humidity",
+//     value: 50,
+//     timestamp: 1628779095
+// }
+// ]
+
+const Display = () => {
+    const [messages, setMessages] = useState<Message[]>([]);
+    const socket = io("http://localhost:5000")
+
     useEffect(() => {
         socket.on("connect", () => {
-            console.log("Connected to server")
+            console.log("Connected to server");
         })
-        socket.on("update", (update: Update) => {
-            console.log(update)
+        socket.on("message", (data: Message) => {
+            console.log(data);
+            setMessages((prevMessages) => [...prevMessages, data])
         })
         return () => {
             socket.disconnect()
         }
-    })
+    }, [socket]);
 
     return (
         <div className="display-wrapper">
-            <h1>Display</h1>
-            <div className="data-stream" >
-                Data stream
-            </div>
+          <h1>Display</h1>
+          <div className="data-stream">
+            {messages.slice().reverse().map((message) => (
+              <div key={message.timestamp} className="message-container">
+                <p>{message.name},</p>
+                <p>{message.value},</p>
+                <p>{message.timestamp}</p>
+              </div>
+            ))}
+          </div>
         </div>
-    )
+      );
 }
+
+export default Display;
