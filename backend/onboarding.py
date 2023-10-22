fe224e85e0a5aa6c0ca1f8d007424f4adc5efcdb
Cherry Lin <pdt8ye@virginia.edu>
2023-10-22 15:14:23 -0400

    made socketio

--- backend/onboarding.py
+++ backend/onboarding.py
@@ -4,12 +4,25 @@ import random
 
 if __name__=="__main__":
     sio = socketio.Server()
+    sio.connect('http://localhost:5000')
     print("done")
 
-while True:
-    num = random.randint(1,100)
-    print(num)
-    time.sleep(1)
+@sio.on('connect')
+def on_connect():
+    while True:
+        num = random.randint(1,100)
+        print(num)
+        time.sleep(1)
+        sio.emit('message', str(num))
 
+@sio.on('disconnect')
+def on_disconnect():
+    print('Disconnected')
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    sio.disconnect()
 
 
